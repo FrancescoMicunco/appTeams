0fc5847b775d903bc85bf2b054b7a7602f5bedb5
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-10-07 16:34:34 +0200

    create a list

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 let addButton = document.getElementById("button-addon2") // button to add names
 let inputName = document.getElementById("inputName")
-
 let teamList = [];
+let listGroup = document.querySelectorAll("ul .list-group")
+let input = inputName.value
 
+function addNameToTheList(input) {
+    let div = document.getElementById("divListName")
+    let ul = document.querySelectorAll("ul .list-group")
+    for (i = 0; i < teamList.length; i++) {
+        let newElement = document.createElement("li")
+        let name = teamList[i];
+        let listname = newElement.innerHTML = name;
+        /* let textInput = document.createTextNode(input)
+        newElement.classList.add("list-group-item"); */
+
+        console.log(listname)
+    }
+    /* name.appendChild(newElement);
+    div.appendChild(ul)
+    console.log(newElement) */
+
+}
+
+
+// grab the input value of element of the list then set to empty this value 
 addButton.addEventListener("click", function(e) {
     teamList.push(inputName.value)
     inputName.value = "";
-
-
-    console.log(teamList)
+    addNameToTheList();
 })
