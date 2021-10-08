47a42b9173033cbe62fa0f8c00f6b997f83f1862
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-10-08 08:48:13 +0200

    fixed bug numbero of team

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ let inputName = document.getElementById("inputName")
 let teamList = [];
 let listGroup = document.querySelectorAll("ul .list-group")
 let input = inputName.value
+let createTableBtn = document.getElementById("button-addon3")
+let createTeamInput = document.getElementById("numOfTeam")
+let numberOfTeam = createTeamInput.value
 
-// grab the input value of element of the list then set to empty this value 
+// ==== grab the input value by the user for the list name and then set to "empty" this field. 
+// ==== Callback to the function that use teamList array
 addButton.addEventListener("click", function(e) {
-    teamList.push(inputName.value)
-    inputName.value = "";
-    addNameToTheList(teamList);
-})
-
-
-
-//create a list of name
+        teamList.push(inputName.value)
+        inputName.value = "";
+        addNameToTheList(teamList);
+    })
+    // ======= create a list of name
+    // ============================
 function addNameToTheList(input) {
-    let div = document.getElementById("divListName")
+    let div = document.querySelector("div#ListName")
     let ul = document.querySelector("ul.list-group")
     ul.classList.add("divListName")
     ul.innerHTML = ""
@@ -25,21 +27,19 @@ function addNameToTheList(input) {
         ul.appendChild(newElement)
     }
 }
-// generate the team table
-let createTableBtn = document.getElementById("button-addon3")
-let createTeamInput = document.getElementById("numOfTeam")
-let numberOfTeam = createTeamInput.value
 
+// =========  generate the team table
+// =====================================
 createTableBtn.addEventListener("click", function(e) {
     let numberOfTeam = createTeamInput.value; // take the value from input of num of teams
     for (let i = 0; i < numberOfTeam; i++) {
-
         let tableTeam = document.querySelector("div#tableTeam");
         let divTeam = document.createElement("div"); //create div container
         divTeam.classList.add(`team`); //create div container
         divTeam.innerHTML = ""; //create div container
         let title = document.createElement("h3");
-        title.innerText = "TEAM n. " + numberOfTeam;
+
+        title.innerText = "TEAM n. " + (i + 1);
         divTeam.appendChild(title);
         tableTeam.appendChild(divTeam)
     }
