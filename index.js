7c5824f2dd333d96a451ec1263d237d8a71fbba7
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-10-09 22:22:44 +0200

    create function for complete teams table

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let addButton = document.getElementById("button-addon2") // button to add names
-let inputName = document.getElementById("inputName")
+let addButton = document.getElementById("button-addon2"); // button to add names
+let inputName = document.getElementById("inputName");
 let teamList = [];
-let listGroup = document.querySelectorAll("ul .list-group")
-let input = inputName.value
-let createTableBtn = document.getElementById("button-addon3")
-let createTeamInput = document.getElementById("numOfTeam")
-let numberOfTeam = createTeamInput.value
-
+let listGroup = document.querySelectorAll("ul .list-group");
+let input = inputName.value;
+let createTableBtn = document.getElementById("button-addon3");
+let createTeamInput = document.getElementById("numOfTeam");
+//let numberOfTeam = createTeamInput.value;
+let assign = document.getElementById("assignBtn");
 // ==== grab the input value by the user for the list name and then set to "empty" this field. 
 // ==== Callback to the function that use teamList array
 addButton.addEventListener("click", function(e) {
@@ -42,4 +42,26 @@ createTableBtn.addEventListener("click", function(e) {
         divTeam.appendChild(title);
         tableTeam.appendChild(divTeam)
     }
-})
+    return numberOfTeam
+});
+
+
+// ========= grab name from the list
+// =====================================
+assign.addEventListener("click", function(e) {
+    let numberOfTeam = createTeamInput.value
+
+    if (numberOfTeam > 0) {
+        let randomElement = Math.floor(Math.random() * (numberOfTeam) + 1);
+        console.log(randomElement);
+        // genera un numero casuale compreso tra 1 e il numero dei numberOfTeam  OK
+        teamList.splice([randomElement - 1], 1);
+        console.log(teamList)
+            // associalo all'elemento dell'array corrispondente al numero generato. OK
+            // prendi l'elemento e inseriscilo in una variabile temporanea
+            //  
+            // trasferiscilo nel primo team libero
+    } else {
+        alert("inserire un numero di team")
+    }
+});
